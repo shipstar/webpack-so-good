30f310b7244174722818915370aabfa89b400e1c
shipstar <kyleashipley@gmail.com>
2017-11-08 11:08:27 -0500

    Add webpack-dashboard to demo a plugin.

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,3 +1,20 @@
 const environment = require('./environment')
+const DashboardPlugin = require('webpack-dashboard/plugin')
+
+environment.plugins.set(
+  'Dashboard',
+  new DashboardPlugin()
+)
 
 module.exports = environment.toWebpackConfig()
+
+// Alternative approach:
+// const customConfig = {
+//   plugins: [
+//     new DashboardPlugin()
+//   ]
+// }
+// module.exports = {
+//   ...environment.toWebpackConfig(),
+//   ...customConfig
+// }
